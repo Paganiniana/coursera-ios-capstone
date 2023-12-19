eb3d2539223cad5c78b0ebb6f860d9827a19fd13
Philip Grabenhorst <phlip007@gmail.com>
2023-12-19 15:10:07 -0800

    Initial Commit

---
+++ LittleLemonFinal/LittleLemonFinalApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonFinalApp.swift
+//  LittleLemonFinal
+//
+//  Created by Philip Grabenhorst on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonFinalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
